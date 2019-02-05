dfcf077d1b17c1c1837853c1c5c66e935658b146
Julian Rios <julianrios@zipcoders-MacBook-Pro.local>
2019-02-05 11:48:45 -0500

    added greeting for Alice or Bob and if it wasn't either

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
 public class Main {
-
+ private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args ){
-
+        System.out.println("Hello please enter your name.");
+        String name = scanner.nextLine();
+        if(name.equals("Alice") || name.equals("Bob")) {
+            System.out.println("Hello " + name + "!"); 
+        } else {
+            System.out.println(name + " you are not Alice or Bob please go away.");
+        }
     }
 }
